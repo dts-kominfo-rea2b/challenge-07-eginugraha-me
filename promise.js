2287c680eb8479e3591316451cf4fa91b6b66e7d
Egi Nugraha <78872708+eginugraha-me@users.noreply.github.com>
2022-06-12 13:08:20 +0700

    challange-07 | commit-01

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const [resIXX, resVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    const result = [...resIXX, ...resVGC]
+    return result?.filter(item => item.hasil === emosi)?.length || 0
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
